90ef85a30372b834579a13d3b6b7d3b018bc48b7
Sakib Hasan <sakibporag24@gmail.com>
2024-12-27 15:45:29 +0600

    Top_Area Asset_Manager

--- lib/src/utils/assets_manager.dart
+++ lib/src/utils/assets_manager.dart
@@ -1,6 +1,13 @@
 class AssetsManager {
   static const String appLogo = '';
+  static const String healthPlusIcon = 'assets/icons/health_plus_icon.svg';
+  static const String healthPlusFillIcon = 'assets/icons/health_plus_fill_icon.svg';
+  static const String starIcon = 'assets/icons/star_icon.svg';
+  static const String starFillIcon = 'assets/icons/star_fill_icon.svg';
+  static const String calendarIcon = 'assets/icons/calendar_icon.svg';
   static const String appArrowRightIcon = 'assets/icons/new_arrow_right_icon.svg';
   static const String authBackground = 'assets/images/background.png';
+  static const String barGraphImage = 'assets/images/bar_graph.svg';
+  static const String notificationIcon = 'assets/icons/notification_icon.svg';
 
 }
